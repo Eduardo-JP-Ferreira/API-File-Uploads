67635dadbe71f60bbf93f035e264f292a7b08f74
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2024-03-03 17:18:46 -0300

    Add multer configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import multer from 'multer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,8 +11,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const upload = multer({ dest: 'uploads/' });
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('Arquivo enviado com sucesso!');
 });
 
 const PORT = process.env.PORT || 8080;
