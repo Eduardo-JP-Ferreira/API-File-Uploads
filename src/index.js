fb7378169f3adae41cf3051a4f56dba736ef7633
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2024-03-03 17:35:46 -0300

    Update upload with file name and extention

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 app.get('/', (req, res) => {
   res.render('index');
