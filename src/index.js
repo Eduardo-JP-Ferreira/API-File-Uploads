ba275b09fa2d59f18dfb0e96df16909625e7eff6
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2024-03-03 17:05:03 -0300

    Create project structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
